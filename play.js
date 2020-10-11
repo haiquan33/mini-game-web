3f73141afffb589e69b46f490d5dd6d4a0f087d2
haiquan33 <haiquan331996@gmail.com>
2020-10-11 15:10:51 +0700

    - update layout

--- play.js
+++ play.js
@@ -23,7 +23,7 @@ let data_1 = [
     url: "google.com",
     imageUrl: "./assets_original/512x384(184).jpg",
     name: "Game5",
-  }
+  },
 ];
 
 let data_2 = [
@@ -56,8 +56,18 @@ let data_2 = [
     url: "google.com",
     imageUrl: "./assets_original/512x384(184).jpg",
     name: "Game5",
-  }
+  },
 ];
+
+let gameDesc = {
+  name: "Game 1",
+  imgUrl: "./assets_original/512x384(10).jpg",
+  embdedCode:
+    '<iframe src="https://freegamemini.com/api/v1/sandbox/game/1027" id="game-player" width="100%" height="100%" frameborder="0" scrolling="no" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+  gameLink: "https://html5.gamemonetize.com/od0553fprdlm105js8cj3y7rgekaumhi/",
+  content:
+    "Spiders is easy to play but tough to master game. You need to pass through the blocky area by swinging on roof and blocks. Avoid the obstacles and collect the gems along your way to unlock fantastic new heroes. Good Luck",
+};
 
 $(document).ready(function () {
   // Back top function
@@ -75,9 +85,11 @@ $(document).ready(function () {
     $("html, body").animate({ scrollTop: 0 }, "300");
   });
 
+  setGameDesc(gameDesc);
+
   setVerticalGameList("list_left", data_1);
   setVerticalGameList("list_right", data_1);
-  setSimilarGameList('similar-list', data_2)
+  setSimilarGameList("similar-list", data_2);
 });
 
 function addGameToList(gameData, listEl) {
@@ -103,4 +115,12 @@ function setSimilarGameList(id, list) {
   list.map((item) => {
     addGameToList(item, gameListEl);
   });
+}
+
+function setGameDesc(desc) {
+  $(".game-detail__desc-name").html(desc.name);
+  $(".game-detail__img").attr("src", desc.imgUrl);
+  $("#embed-code").val(desc.embdedCode);
+  $("#game-link").val(desc.gameLink);
+  $("#desc-content").html(desc.content);
 }
